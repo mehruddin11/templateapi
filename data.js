17c21c9de7b5209faa860b86719b7ae978376c08
mehruddin11 <mehruddin881@gmail.com>
2022-02-09 12:12:42 +0530

    adding api

--- data.js
+++ data.js
@@ -2,21 +2,21 @@ const data = {
     Templates:[
     {
         id:'1',
-        homepage:'birthdayreminder1.netlify.app/',
+        homepage:'https://birthdayreminder1.netlify.app/',
         html_url:'https://github.com/mehruddin11/birthdayReminder',
         download:'./Database/birthday.zip',
         img:''
     },
     {
         id:'2',
-        homepage:'auto-slider.netlify.app',
+        homepage:'https://auto-slider.netlify.app/',
         html_url:'https://github.com/mehruddin11/slider',
         download:'./Database/todolist.zip',
         img:''
     },
     {
         id:'3',
-        homepage:'grocery-bud-mehruddin.netlify.app',
+        homepage:'https://grocery-bud-mehruddin.netlify.app/',
         html_url:'https://github.com/mehruddin11/grocery-item',
         download:'./Database/slider-main.zip',
         img:''
